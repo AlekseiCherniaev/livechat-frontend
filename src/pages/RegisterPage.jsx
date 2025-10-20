54b217d2fab88bb2c8266b3d21186d251a56a356
Aleksey Cherniaev <cherniaevaleksey2304@gmail.com>
2025-10-20 21:10:28 +0300

    feat: add user register page

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,10 +1,8 @@
 import { useState } from 'react';
 import { useAuth } from '../context/AuthContext';
-import { useNavigate } from 'react-router-dom';
 
 export default function RegisterPage() {
   const { register } = useAuth();
-  const navigate = useNavigate();
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState(null);
